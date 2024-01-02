cde2df0535e4ab19b63b774e9aaf332375ee12dd
Sam Williamson <swill@getfarmlab.com>
2024-01-03 10:22:47 +1100

    Inited quasar

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <router-view />
+</template>
+
+<script lang="ts">
+import { useQuasar } from 'quasar';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+  setup() {
+    const $q = useQuasar();
+
+    $q.dark.set(false);
+  },
+});
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,56 @@
+<template>
+  <q-card flat bordered class="my-card">
+    <q-card-section>
+      <div class="text-h6">Our Changing Planet</div>
+    </q-card-section>
+
+    <q-card-section class="q-pt-none">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua.
+    </q-card-section>
+
+    <q-separator inset />
+
+    <q-card-section>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua.
+    </q-card-section>
+  </q-card>
+</template>
+
+<script lang="ts">
+import { Meta, Todo } from 'components/models';
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: 'IndexPage',
+  setup() {
+    const todos = ref<Todo[]>([
+      {
+        id: 1,
+        content: 'ct1',
+      },
+      {
+        id: 2,
+        content: 'ct2',
+      },
+      {
+        id: 3,
+        content: 'ct3',
+      },
+      {
+        id: 4,
+        content: 'ct4',
+      },
+      {
+        id: 5,
+        content: 'ct5',
+      },
+    ]);
+    const meta = ref<Meta>({
+      totalCount: 1200,
+    });
+    return { todos, meta };
+  },
+});
+</script>
